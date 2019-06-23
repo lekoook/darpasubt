dbaf0871b8899e4480e48bd21180fb981ba2b6ec
lekoook <kokteng1313@gmail.com>
2019-06-23 19:10:22 +0800

    Add new ranging technique algorithm
    This branch aims to experiment and test with another ranging
    technique using the DWM1001.
    
    At the core, this technique uses multiple acknowledgement frames
    in an effort to reduce the total number of frames transacted and
    improve overall accuracy.
    
    A lower transacted frames will reduce the power consumption of the
    nodes.

---
+++ firmware/dwm1001_dev_source_code/development/ds_twr_ma_init/ds_twr_ma_init_main.h
@@ -0,0 +1,22 @@
+#define OTP_ANT_DLY 0x01C
+// #define USE_SS_TWR // Uncomment to use SS_TWR technique.
+#define USE_DS_TWR_MA
+
+/* Declaration of static functions. */
+static void resp_msg_get_ts(uint8 *ts_field, uint32 *ts);
+
+int ss_init_run(void);
+
+void rx_ok_cb(const dwt_cb_data_t *cb_data);
+
+void rx_to_cb(const dwt_cb_data_t *cb_data);
+
+void rx_err_cb(const dwt_cb_data_t *cb_data);
+
+void tx_conf_cb(const dwt_cb_data_t *cb_data);
+
+static void resp_msg_get_ts(uint8 *ts_field, uint32 *ts);
+
+void ss_initiator_task_function (void * pvParameter);
+
+static uint16 getAntDly(uint8 prf);
---
+++ firmware/dwm1001_dev_source_code/development/ds_twr_ma_init/main.c
@@ -0,0 +1,275 @@
+/* Copyright (c) 2015 Nordic Semiconductor. All Rights Reserved.
+ *
+ * The information contained herein is property of Nordic Semiconductor ASA.
+ * Terms and conditions of usage are described in detail in NORDIC
+ * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information. NO
+ * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
+ * the file.
+ *
+ */
+
+#include "sdk_config.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+#include "bsp.h"
+#include "boards.h"
+#include "nordic_common.h"
+#include "nrf_drv_clock.h"
+#include "nrf_drv_spi.h"
+#include "nrf_uart.h"
+#include "app_util_platform.h"
+#include "nrf_gpio.h"
+#include "nrf_delay.h"
+#include "nrf_log.h"
+#include "nrf.h"
+#include "app_error.h"
+#include "app_util_platform.h"
+#include "app_error.h"
+#include <string.h>
+#include "port_platform.h"
+#include "deca_types.h"
+#include "deca_param_types.h"
+#include "deca_regs.h"
+#include "deca_device_api.h"
+#include "uart.h"
+#include "ds_twr_ma_init_main.h"
+#include "nrf_drv_gpiote.h"
+	
+//-----------------dw1000----------------------------
+
+/*DW1000 config function*/
+static dwt_config_t config = {
+    5,                /* Channel number. */
+    DWT_PRF_64M,      /* Pulse repetition frequency. */
+    DWT_PLEN_128,     /* Preamble length. Used in TX only. */
+    DWT_PAC8,         /* Preamble acquisition chunk size. Used in RX only. */
+    10,               /* TX preamble code. Used in TX only. */
+    10,               /* RX preamble code. Used in RX only. */
+    0,                /* 0 to use standard SFD, 1 to use non-standard SFD. */
+    DWT_BR_6M8,       /* Data rate. */
+    DWT_PHRMODE_STD,  /* PHY header mode. */
+    (129 + 8 - 8)     /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
+};
+
+/* Preamble timeout, in multiple of PAC size. See NOTE 3 below. */
+#define PRE_TIMEOUT 1000
+
+/* Delay between frames, in UWB microseconds. See NOTE 1 below. */
+#define POLL_TX_TO_RESP_RX_DLY_UUS 100 
+
+//--------------dw1000---end---------------
+
+
+#define TASK_DELAY        200           /**< Task delay. Delays a LED0 task for 200 ms */
+#define TIMER_PERIOD      2000          /**< Timer period. LED1 timer will expire after 1000 ms */
+
+#ifdef USE_FREERTOS
+
+TaskHandle_t  ss_initiator_task_handle;   /**< Reference to SS TWR Initiator FreeRTOS task. */
+extern void ss_initiator_task_function (void * pvParameter);
+TaskHandle_t  led_toggle_task_handle;   /**< Reference to LED0 toggling FreeRTOS task. */
+TimerHandle_t led_toggle_timer_handle;  /**< Reference to LED1 toggling FreeRTOS timer. */
+#endif
+
+#ifdef USE_FREERTOS
+
+/**@brief LED0 task entry function.
+ *
+ * @param[in] pvParameter   Pointer that will be used as the parameter for the task.
+ */
+static void led_toggle_task_function (void * pvParameter)
+{
+  UNUSED_PARAMETER(pvParameter);
+  while (true)
+  {
+    LEDS_INVERT(BSP_LED_0_MASK);
+    /* Delay a task for a given number of ticks */
+    vTaskDelay(TASK_DELAY);
+    /* Tasks must be implemented to never return... */
+  }
+}
+
+/**@brief The function to call when the LED1 FreeRTOS timer expires.
+ *
+ * @param[in] pvParameter   Pointer that will be used as the parameter for the timer.
+ */
+static void led_toggle_timer_callback (void * pvParameter)
+{
+  UNUSED_PARAMETER(pvParameter);
+  LEDS_INVERT(BSP_LED_1_MASK);
+}
+#else
+
+  extern int ss_init_run(void);
+
+#endif   // #ifdef USE_FREERTOS
+
+// Global variables
+uint16 antDelay;
+
+
+int main(void)
+{
+  /* Setup some LEDs for debug Green and Blue on DWM1001-DEV */
+  LEDS_CONFIGURE(BSP_LED_0_MASK | BSP_LED_1_MASK | BSP_LED_2_MASK);
+  LEDS_ON(BSP_LED_0_MASK | BSP_LED_1_MASK | BSP_LED_2_MASK );
+
+  #ifdef USE_FREERTOS
+    /* Create task for LED0 blinking with priority set to 2 */
+    UNUSED_VARIABLE(xTaskCreate(led_toggle_task_function, "LED0", configMINIMAL_STACK_SIZE + 200, NULL, 2, &led_toggle_task_handle));
+
+    /* Start timer for LED1 blinking */
+    led_toggle_timer_handle = xTimerCreate( "LED1", TIMER_PERIOD, pdTRUE, NULL, led_toggle_timer_callback);
+    UNUSED_VARIABLE(xTimerStart(led_toggle_timer_handle, 0));
+
+    /* Create task for SS TWR Initiator set to 2 */
+    UNUSED_VARIABLE(xTaskCreate(ss_initiator_task_function, "SSTWR_INIT", configMINIMAL_STACK_SIZE + 200, NULL, 2, &ss_initiator_task_handle));
+  #endif // #ifdef USE_FREERTOS
+  
+  //-------------dw1000  ini------------------------------------	
+
+  /* Setup NRF52832 interrupt on GPIO 25 : connected to DW1000 IRQ*/
+  vInterruptInit();
+	
+  /*Initialization UART*/
+  boUART_Init ();
+  printf("Initiator - Double Sided Two Way Ranging Multiple Acknowledgements \r\n");
+	
+  /* Reset DW1000 */
+  reset_DW1000(); 
+
+  /* Set SPI clock to 2MHz */
+  port_set_dw1000_slowrate();			
+  
+  /* Init the DW1000 */
+  if (dwt_initialise(DWT_LOADUCODE) == DWT_ERROR)
+  {
+    //Init of DW1000 Failed
+    while (1) {};
+  }
+
+  // We need to read antenna delay in OTP memory with slow SPI clock.
+  // We set it and NEVER change it, as it will be used for all future timestamping purpose.
+  antDelay = getAntDly(DWT_PRF_64M);
+  dwt_settxantennadelay(antDelay);
+  dwt_setrxantennadelay(antDelay);
+
+  // Set SPI to 8MHz clock  
+  port_set_dw1000_fastrate();
+
+  /* Configure DW1000. */
+  dwt_configure(&config);
+
+  /* Initialization of the DW1000 interrupt*/
+  /* Callback are defined in ds_twr_init_main.c */
+  dwt_setcallbacks(&tx_conf_cb, &rx_ok_cb, &rx_to_cb, &rx_err_cb);
+
+  /* Enable wanted interrupts (TX confirmation, RX good frames, RX timeouts and RX errors). */
+  dwt_setinterrupt(DWT_INT_TFRS | DWT_INT_RFCG | DWT_INT_RFTO | DWT_INT_RXPTO | DWT_INT_RPHE | DWT_INT_RFCE | DWT_INT_RFSL | DWT_INT_SFDT, 1);
+
+  /* Set preamble timeout for expected frames. See NOTE 3 below. */
+  //dwt_setpreambledetecttimeout(0); // PRE_TIMEOUT
+          
+  /* Set expected response's delay and timeout. 
+  * As this example only handles one incoming frame with always the same delay and timeout, those values can be set here once for all. */
+  dwt_setrxaftertxdelay(POLL_TX_TO_RESP_RX_DLY_UUS);
+  dwt_setrxtimeout(65000); // Disable RX timeout.
+
+  //-------------dw1000  ini------end---------------------------	
+  // IF WE GET HERE THEN THE LEDS WILL BLINK
+
+  #ifdef USE_FREERTOS		
+    /* Start FreeRTOS scheduler. */
+    vTaskStartScheduler();	
+      while(1)
+      {};
+  #else
+    // No RTOS task here so just call the main loop here.
+    // Loop forever responding to ranging requests.
+      while (1)
+      {
+        ds_twr_ma_run();
+      }
+		
+  #endif
+		
+}
+
+/*DWM1000 interrupt initialization and handler definition*/
+
+/*!
+* Interrupt handler calls the DW1000 ISR API. Call back corresponding to each event defined in ss_init_main
+*/
+void vInterruptHandler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
+{
+  dwt_isr(); // DW1000 interrupt service routine 
+}
+
+/*!
+* @brief Configure an IO pin as a positive edge triggered interrupt source.
+*/
+void vInterruptInit (void)
+{
+  ret_code_t err_code;
+
+  if (nrf_drv_gpiote_is_init())
+    printf("nrf_drv_gpiote_init already installed\n");
+  else
+    nrf_drv_gpiote_init();
+
+  // input pin, +ve edge interrupt, no pull-up
+  nrf_drv_gpiote_in_config_t in_config = GPIOTE_CONFIG_IN_SENSE_LOTOHI(true);
+  in_config.pull = NRF_GPIO_PIN_NOPULL;
+
+  // Link this pin interrupt source to its interrupt handler
+  err_code = nrf_drv_gpiote_in_init(DW1000_IRQ, &in_config, vInterruptHandler);
+  APP_ERROR_CHECK(err_code);
+
+  nrf_drv_gpiote_in_event_enable(DW1000_IRQ, true);
+}
+
+/**
+ * @brief Retrieves the antenna delay stored in OTP memory.
+ * 
+ * @param prf the PRF used to configure DWM1000.
+ * @return antenna delay from OTP memory.
+*/
+static uint16 getAntDly(uint8 prf)
+{
+  uint32 rawAntDelay;
+  uint16 antDelay = 0;
+  dwt_otpread(OTP_ANT_DLY, &rawAntDelay, 1); // '1' Refers to one 32 bits word.
+  if (prf == DWT_PRF_16M)
+  {
+    antDelay = rawAntDelay & 0x0000FFFF; // PRF 16M Antenna delay is found in the lower 16 bits.
+  }
+  else if (prf == DWT_PRF_64M)
+  {
+    antDelay = rawAntDelay >> 16;
+  }
+  return antDelay;
+}
+
+
+/*****************************************************************************************************************************************************
+ * NOTES:
+ *
+ * 1. The single-sided two-way ranging scheme implemented here has to be considered carefully as the accuracy of the distance measured is highly
+ *    sensitive to the clock offset error between the devices and the length of the response delay between frames. To achieve the best possible
+ *    accuracy, this response delay must be kept as low as possible. In order to do so, 6.8 Mbps data rate is used in this example and the response
+ *    delay between frames is defined as low as possible. The user is referred to User Manual for more details about the single-sided two-way ranging
+ *    process.  NB:SEE ALSO NOTE 11.
+ * 3. This timeout is for complete reception of a frame, i.e. timeout duration must take into account the length of the expected frame. Here the value
+ *    is arbitrary but chosen large enough to make sure that there is enough time to receive the complete response frame sent by the responder at the
+ *    6.8M data rate used (around 200 µs).
+ * 4. In a real application, for optimum performance within regulatory limits, it may be necessary to set TX pulse bandwidth and TX power, (using
+ *    the dwt_configuretxrf API call) to per device calibrated values saved in the target system or the DW1000 OTP memory.
+ * 5. The user is referred to DecaRanging ARM application (distributed with EVK1000 product) for additional practical example of usage, and to the
+ *     DW1000 API Guide for more details on the DW1000 driver functions.
+ *
+ ****************************************************************************************************************************************************/
+
+
