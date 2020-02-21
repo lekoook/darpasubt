0ff36b4ccaf764d4e2c4b8e3eb48d31a6d0d79ca
lekoook <kokteng1313@gmail.com>
2020-02-20 18:38:15 -0800

    Remove rosserial usage for udoo leonardo

--- firmware/udoo_leonardo/src/main.cpp
+++ firmware/udoo_leonardo/src/main.cpp
@@ -1,51 +1,30 @@
-#define USE_USBCON
 #define NUM_SERVOS 3
 
 #include <HCSR04.h>
-#include <ros.h>
-#include <sensor_msgs/Range.h>
-#include <std_msgs/UInt16.h>
 #include <math.h>
 #include <Wire.h>
 #include "SparkFun_SCD30_Arduino_Library.h"
-#include <Dropper.h>
 #include <Servo.h>
 
-void dropperCb(const vehicle_drive::Dropper& angles);
+UltraSonicDistanceSensor distanceSensor(11, 12);
 
-sensor_msgs::Range rangeMsg;
-std_msgs::UInt16 co2Msg;
-ros::NodeHandle nh;
-ros::Publisher rangePub("ultrasonic", &rangeMsg);
-ros::Publisher co2Pub("co2", &co2Msg);
-ros::Subscriber<vehicle_drive::Dropper> droppersPub("droppers", dropperCb);
-UltraSonicDistanceSensor distanceSensor(0, 1);
-SCD30 airSensor;
+//SCD30 airSensor;
 
 Servo servosArr[NUM_SERVOS];
 int servosPin[NUM_SERVOS] = {5, 6, 9};
 
-void dropperCb(const vehicle_drive::Dropper& angles)
-{
-    for (int i = 0; i < NUM_SERVOS; i++)
-    {
-        servosArr[i].write(angles.dropper_angles[i]);
-    }
-}
-
 void setup () {
-    nh.initNode();
-    nh.advertise(rangePub);
-    nh.advertise(co2Pub);
-    nh.subscribe(droppersPub);
-    Wire.begin();
-    airSensor.begin();
+//    Wire.begin();
+    Serial.begin(9600);
+
+//    airSensor.begin();
 
     for (int i = 0; i < NUM_SERVOS; i++)
     {
         servosArr[i].attach(servosPin[i]);
         servosArr[i].write(60);
     }
+
 }
 
 void loop () {
@@ -58,15 +37,13 @@ void loop () {
     {
         dist = dist / 100.0;
     }
-    rangeMsg.range = dist;
-    rangePub.publish(&rangeMsg);
-
+    Serial.println(dist);
+/*
     if (airSensor.dataAvailable())
     {
-        co2Msg.data = airSensor.getCO2();
-        co2Pub.publish(&co2Msg);
+	double co2 = airSensor.getCO2();
+        Serial.println(co2);
     }
-    
-    nh.spinOnce();
+*/
     delay(100);
 }
