5996260bbb595394e40a19cee5e7fcb3a51b7028
lekoook <kokteng1313@gmail.com>
2020-02-24 06:55:28 +0700

    Modify udoo dropper, ultrasonic, co2 code
    Modify to work with Teensy 4.0 boards

--- firmware/udoo_leonardo/src/main.cpp
+++ firmware/udoo_leonardo/src/main.cpp
@@ -1,28 +1,64 @@
+// Check which board is used. Leonardo or Teensy
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega16U4__)
+#define USING_LEONARDO
+#endif
+#ifdef CORE_TEENSY
+#define USING_TEENSY
+#endif
+
+
+#ifdef USING_LEONARDO
+#define D_PIN_1 5
+#define D_PIN_2 6
+#define D_PIN_3 9
 #define USE_USBCON
+#include <Servo.h>
+#endif
+
+
+#ifdef USING_TEENSY
+#define D_PIN_1 3
+#define D_PIN_2 4
+#define D_PIN_3 5
+#include <PWMServo.h>
+#include <Wire.h>
+#include "SparkFun_SCD30_Arduino_Library.h"
+#include <sensor_msgs/Range.h>
+#include <std_msgs/UInt16.h>
+#endif
+
+
 #define NUM_SERVOS 3
 
 #include <HCSR04.h>
 #include <math.h>
-//#include <Wire.h>
-//#include "SparkFun_SCD30_Arduino_Library.h"
-#include <Servo.h>
 #include <ros.h>
-//#include <sensor_msgs/Range.h>
 #include <std_msgs/Float32.h>
 #include <Dropper.h>
 
+// Function prototypes
 void dropperCb(const vehicle_drive::Dropper& angles);
 
+// Common global declarations for all boards
 std_msgs::Float32 rangeMsg;
-//std_msgs::UInt16 co2Msg;
 ros::NodeHandle nh;
 ros::Publisher rangePub("ultrasonic", &rangeMsg);
-//ros::Publisher co2Pub("co2", &co2Msg);
 ros::Subscriber<vehicle_drive::Dropper> droppersPub("droppers", dropperCb);
-UltraSonicDistanceSensor distanceSensor(11, 12);
-//SCD30 airSensor;
 
+// Global declaration for Leonardo board.
+#ifdef USING_LEONARDO
 Servo servosArr[NUM_SERVOS];
+UltraSonicDistanceSensor distanceSensor(11, 12);
+#endif
+
+// Global declaration for Teensy board.
+#ifdef USING_TEENSY
+PWMServo servosArr[NUM_SERVOS];
+std_msgs::UInt16 co2Msg;
+ros::Publisher co2Pub("co2", &co2Msg);
+SCD30 airSensor;
+UltraSonicDistanceSensor distanceSensor(9, 10);
+#endif
 
 void dropperCb(const vehicle_drive::Dropper& angles)
 {
@@ -35,12 +71,15 @@ void dropperCb(const vehicle_drive::Dropper& angles)
 void setup () {
     nh.initNode();
     nh.advertise(rangePub);
-//    nh.advertise(co2Pub);
     nh.subscribe(droppersPub);
-//    Wire.begin();
-//    airSensor.begin();
 
-    int servosPin[NUM_SERVOS] = {5, 6, 9};
+    #ifdef USING_TEENSY
+    nh.advertise(co2Pub);
+    Wire.begin();
+    airSensor.begin();
+    #endif
+
+    int servosPin[NUM_SERVOS] = {D_PIN_1, D_PIN_2, D_PIN_3};
 
     for (int i = 0; i < NUM_SERVOS; i++)
     {
@@ -63,13 +102,16 @@ void loop () {
     }
     rangeMsg.data = dist;
     rangePub.publish(&rangeMsg);
-/*
+
+    #ifdef USING_TEENSY
     if (airSensor.dataAvailable())
     {
-	double co2 = airSensor.getCO2();
-        Serial.println(co2);
+	    double co2 = airSensor.getCO2();
+        co2Msg.data = co2;
+        co2Pub.publish(&co2Msg);
     }
-*/    
+    #endif
+
     nh.spinOnce();
     delay(100);
 }
